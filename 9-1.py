80b96da1604b864f16759a1637685d0c4ce1d661
13372632044 <49483051+13372632044@users.noreply.github.com>
2020-04-18 18:39:14 +0800

    Add files via upload

--- 9-1.py
+++ 9-1.py
@@ -1,33 +1,95 @@
-import numpy as np
 import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
 
-x1 = tf.constant([137.97, 104.50, 100.00, 124.32, 79.20, 99.00, 124.00, 114.00,106.69, 138.05, 53.75, 46.91, 68.00, 63.02, 81.26, 86.21])
-x2 = tf.constant([3,2,2,3, 1,2,3,2,2,3, 1,1, 1, 1,2,2],dtype=tf.float32)
-y = tf.constant([145.00, 110.00, 93.00, 116.00, 65.32, 104.00, 118.00, 91.00,62.00, 133.00, 51.00, 45.00, 78.50, 69.65, 75.69, 95.30])
-x0 = tf.ones(len(x1))
 
-X = tf.stack((x0,x1,x2),axis=1)
-Y = tf.reshape(y,[-1,1])
+import pandas as pd
+from sklearn.utils import shuffle
+from sklearn.preprocessing import scale
 
-Xt = tf.transpose(X)
-XtX_1 = tf.linalg.inv(Xt@X)
-XtX_1_Xt = XtX_1@Xt
-w = XtX_1_Xt@Y
+df = pd.read_csv("C:/Users/SSL98/Desktop/神经网络和深度学习作业文件/第七周作业/data/boston.csv",header=0)
+print(df.describe())
 
-W = tf.reshape(w,[-1]).numpy()
-print("多元线性回归方程：")
-print("Y=",W[1],"*x1+",W[2],"*x2+",W[0])
+ds=df.values
+print(ds.shape)
+print(ds)
+df.head(3)
+df.tail(3)
 
-print("--------------------------")
-print("请输入房屋面积和房间数，预测房屋销售价格：")
-x1_test = float(input("商品房面积："))
-while x1_test<20 or x1_test>500:
-    print("您输入的数据不对,请重新输入")
-    x1_test = float(input("商品房面积："))
-x2_test = int(input("房间数："))
-while x2_test<1 or x2_test>10:
-    print("您输入的数据不对,请重新输入")
-    x2_test = int(input("房间数："))
+#x_data 为归一化后的前12列特征数据
+x_data = ds[:,:12]
 
-y_pred = W[1]*x1_test+W[2]*x2_test+W[0]
-print("预测价格为：",round(y_pred,2),"万元") 
+#y_data为最后一列数据
+y_data = ds[:,12]
+
+for i in range(12):
+    x_data[:,i]=(x_data[:,i]-x_data[:,i].min())/(x_data[:,i].max()-x_data[:,i].min())
+
+print('x_data shape=',x_data.shape)
+print('y_data shape=',y_data.shape)
+
+train_num = 300   #训练集数目
+valid_num = 100   #验证集数目
+test_num = len(x_data)-train_num-valid_num   #测试集数目
+
+#训练集划分
+x_train = x_data[:train_num]
+y_train = y_data[:train_num]
+
+#验证集划分
+x_valid = x_data[train_num:train_num+valid_num]
+y_valid = y_data[train_num:train_num+valid_num]
+
+#测试集划分
+x_test = x_data[train_num+valid_num:train_num+valid_num+test_num]
+y_test = y_data[train_num+valid_num:train_num+valid_num+test_num]
+
+x_train = tf.cast(scale(x_train),dtype=tf.float32)
+x_valid = tf.cast(scale(x_valid),dtype=tf.float32)
+x_test = tf.cast(scale(x_test),dtype=tf.float32)
+
+def model(x,w,b):
+    return tf.matmul(x,w)+b
+
+W=tf.Variable(tf.random.normal([12,1],mean=0.0,stddev=1.0,dtype=tf.float32))
+B=tf.Variable(tf.zeros(1),dtype=tf.float32)
+
+print(W)
+print(B)
+
+training_epochs = 50 #迭代次数
+learning_rate = 0.001 #学习率
+batch_size = 10 #批量训练一次的样本数
+
+#采用均方差作为损失函数
+def loss(x,y,w,b):
+    err = model(x,w,b) - y
+    squared_err = tf.square(err)
+    return tf.reduce_mean(squared_err) #均方差
+
+#计算样本数据[x,y]在参数[w,b]点上的梯度
+def grad(x,y,w,b):
+    with tf.GradientTape() as tape:
+        loss_ = loss(x,y,w,b)
+    return tape.gradient(loss_, [w,b]) #返回梯度向量
+
+optimizer = tf.keras.optimizers.SGD(learning_rate) #创建优化器，指定学习率
+
+loss_list_train = [] #用于保存训练集loss值的列表
+loss_list_valid = [] #用于保存验证集loss值的列表
+total_step = int(train_num/batch_size)
+
+for epoch in range (training_epochs):
+    for step in range(total_step):
+        xs = x_train[step*batch_size:(step+1)*batch_size,:]
+        ys = y_train[step*batch_size:(step+1)*batch_size]
+        
+        grads = grad(xs,ys,W,B)
+        optimizer.apply_gradients(zip(grads,[W,B])) 
+        
+    loss_train = loss(x_train, y_train, W, B).numpy()
+    loss_valid = loss(x_valid, y_valid, W, B).numpy()
+    loss_list_train.append(loss_train)
+    loss_list_valid.append(loss_valid)
+    print("epoch={:3d} ,train_loss={:.4f},valid_loss={:.4f}".format(epoch+1,loss_train,loss_valid))
+
